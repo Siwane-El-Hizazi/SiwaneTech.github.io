e443d568b7ee8a4a26f135424d283f1df7da32e2
Jamal El Hizazi <elhizazi1@gmail.com>
2024-04-13 11:17:45 +0000

    Update ripple.js
    Signed-off-by: Jamal El Hizazi <elhizazi1@gmail.com>

--- codes/ripple.js
+++ codes/ripple.js
@@ -1,10 +1,10 @@
 document.addEventListener('click', function(event) {
-        var ripple = document.createElement('div');
-        ripple.className = 'ripple-effect';
-        ripple.style.left = event.clientX - 10.8 + 'px';
-        ripple.style.top = event.clientY - 13 + 'px';
-        document.body.appendChild(ripple);
-        setTimeout(function() {
-            ripple.remove();
-        }, 1000);
-    });
+    var ripple = document.createElement('div');
+    ripple.className = 'ripple-effect';
+    ripple.style.left = event.clientX + 'px';
+    ripple.style.top = event.clientY + 'px';
+    document.documentElement.appendChild(ripple);
+    setTimeout(function() {
+        ripple.remove();
+    }, 1000);
+});
